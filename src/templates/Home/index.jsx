6b057c2c58597efc0fc862128e3fed6d6dedb6e7
Wander Areosa <wander.areosa@gmail.com>
2022-10-19 16:37:17 -0400

    10 posts por pagina

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -11,7 +11,7 @@ export class Home extends Component {
     posts:[],
     allPosts:[],
     page: 0,
-    postsPerPage: 5,
+    postsPerPage: 10,
     searchValue: ''
   }
   async componentDidMount(){
